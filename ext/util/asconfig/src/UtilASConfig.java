0c93165da56bfa45309285abdaf3d4f3cf2580ec
LinkWu <wjianl@qq.com>
2018-08-13 11:33:44 +0800

    调整

--- ext/util/asconfig/src/UtilASConfig.java
+++ ext/util/asconfig/src/UtilASConfig.java
@@ -1,71 +1,83 @@
+import java.awt.datatransfer.StringSelection;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.CharBuffer;
+import java.util.ArrayList;
 import java.util.UUID;
-
+import org.apache.commons.lang3.StringUtils;
 
 public class UtilASConfig {
 	private static final String SRC_SRT = "\"../TARGET_NAME/src\",";
-	
-	private String _path;
-	
-	public UtilASConfig(String path) {
-		_path = path;
+
+	private String _root_path;
+	private String _project_path;
+
+	private ArrayList<String> _array;
+
+	public UtilASConfig(String root_path, String project_path) {
+		_root_path = root_path;
+		_project_path = project_path;
+		_array = new ArrayList<String>();
 	}
-	
-	public void done() throws Exception{
-		CharBuffer buffer = CharBuffer.allocate(4096);
-		
-		String content;
-		String[] array;
-		
-		content = this.readContent(_path + "/.dependlib");
-		array = content.split(",");
+
+	public void done() throws Exception {
+		this.collectAll(_project_path);
+
+		String r_content = this.readContent(_project_path + "/asconfig.json");
+
+		r_content = r_content.replaceAll("@@external_src@@",
+				StringUtils.join(_array.toArray(), ""));
+
+		this.writeContent(_project_path + "/asconfig.json", r_content);
+	}
+
+	private void collectAll(String path) throws Exception {
+		this.collect(path + "/.dependlib");
+		this.collect(path + "/.dependrsl");
+	}
+
+	private void collect(String file) throws Exception {
+		String content = this.readContent(file);
+		String[] array = content.split(",");
 		for (String str : array) {
-			if(null != str && !"".equals(str.trim())){
-				buffer.put(SRC_SRT.replaceAll("TARGET_NAME", str.trim()));
+			str = str.trim();
+
+			if (null == str || "".equals(str)) {
+				continue;
 			}
+
+			this.collectAll(_root_path + "/" + str);
+
+			str = "\"../" + str + "/src\",";
+
+			if (_array.contains(str)) {
+				continue;
+			}
+
+			_array.add(str);
 		}
-		
-		content = this.readContent(_path + "/.dependrsl");
-		array = content.split(",");
-		for (String str : array) {
-			if(null != str && !"".equals(str.trim())){
-				buffer.put(SRC_SRT.replaceAll("TARGET_NAME", str.trim()));
-			}	
-		}
-		
-		String r_content = this.readContent(_path + "/asconfig.json");
-
-
-		r_content = r_content.replaceAll("@@external_src@@", buffer.flip().toString());
-
-		this.writeContent(_path + "/asconfig.json", r_content);
-		
-		buffer.clear();
 	}
-	
 
-	private String readContent(String fileName) throws Exception{
+	private String readContent(String fileName) throws Exception {
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
 		CharBuffer cb = CharBuffer.allocate(4096);
 		br.read(cb);
 		br.close();
-		
+
 		String result = cb.flip().toString();
 		cb.clear();
 		return result;
 	}
-	
-	private void writeContent(String fileName, String content) throws Exception{
+
+	private void writeContent(String fileName, String content) throws Exception {
 		BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
 		bw.write(content);
 		bw.close();
 	}
-	
+
 	public static void main(String[] args) throws Exception {
-		new UtilASConfig(args[0]).done();
+		new UtilASConfig(args[0], args[1]).done();
 	}
 }
